444a2b3213b9366233851422f0e3cc853af00773
Dylan Grozdanich <dgrozdanich@gmail.com>
2017-01-23 16:31:48 -0800

    Set up view controller and basic UI

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Dylan Grozdanich on 1/23/17.
+//  Copyright © 2017 Dylan Grozdanich. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+}

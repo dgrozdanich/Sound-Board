f2ab5196ec1c85905ca7fefd527bf1c76c3e6e52
Dylan <dgrozdanich@gmail.com>
2017-01-26 23:25:33 -0800

    Yay! Got the recorder working

--- Sound Board/SoundViewController.swift
+++ Sound Board/SoundViewController.swift
@@ -9,18 +9,19 @@
 import UIKit
 import AVFoundation
 
-class SoundViewController: UIViewController {
+class SoundViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     
     @IBOutlet weak var recordButton: UIButton!
     
     @IBOutlet weak var textField: UITextField!
     
     
-    var audioRecorder : AVAudioRecorder? = nil
+    var audioRecorder : AVAudioRecorder!
+    var soundPlayer : AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+         setupRecorder()
         // Do any additional setup after loading the view.
     }
     
@@ -41,11 +42,14 @@ class SoundViewController: UIViewController {
             let pathComponents = [basePath, "audio.m4a"]
             
             let audioURL = NSURL.fileURL(withPathComponents: pathComponents)
+            print("########################")
+            print(audioURL!)
+            print("#########################")
             
         // Create settings for the audio recordered
         
             var settings : [String:AnyObject] = [:]
-             settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
             settings[AVSampleRateKey] = 44100.0 as AnyObject?
             settings[AVNumberOfChannelsKey] = 2 as AnyObject?
             
@@ -54,7 +58,7 @@ class SoundViewController: UIViewController {
         
         audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
             
-            audioRecorder!.prepareToRecord()
+            audioRecorder.prepareToRecord()
             
         } catch let error as NSError {
             print(error)
@@ -65,6 +69,21 @@ class SoundViewController: UIViewController {
     
     
     @IBAction func recordTapped(_ sender: Any) {
+        
+        if audioRecorder.isRecording {
+            // Stop the Recording
+            audioRecorder.stop()
+            // Change button title to record
+            recordButton.setTitle("Record", for: .normal)
+        } else {
+            // Start the Recording
+            audioRecorder.record()
+            
+            // Change button title to stop
+            recordButton.setTitle("Stop", for: .normal)
+            
+        }
+        
     }
 
     @IBAction func playTapped(_ sender: Any) {
